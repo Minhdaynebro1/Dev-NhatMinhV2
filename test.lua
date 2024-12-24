4679c661817b5760288c70ebde0b614187f5987c
Minhdaynebro1 <164420001+Minhdaynebro1@users.noreply.github.com>
2024-12-25 02:50:54 +0800

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 16480898254 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinhV2/refs/heads/main/Eat%20The%20World.lua"))();
+else
+  game.Players.LocalPlayer:Kick("Game is not supported!");
+end;
